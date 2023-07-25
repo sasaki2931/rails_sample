5715171bec0749f2b65f070cc5062dba92b7ee92
sasaki <sasaki2931@gmail.com>
2023-07-25 03:27:40 +0000

    Add account activation

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,17 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at
+  # http://https://f247cd0b4d3a419fad71e6bf8cb4adc3.vfs.cloud9.ap-northeast-1.amazonaws.com//rails/mailers/user_mailer/account_activation
+  def account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+  end
+
+  # Preview this email at
+  # http://https://f247cd0b4d3a419fad71e6bf8cb4adc3.vfs.cloud9.ap-northeast-1.amazonaws.com//rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
+end

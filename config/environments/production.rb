c833dd694b02d0bf858661687f4ed20906ea7f84
sasaki <sasaki2931@gmail.com>
2023-07-25 07:29:26 +0000

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'rails-sample-f08v.onrender.com'
+  host = 'rails-sample.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,

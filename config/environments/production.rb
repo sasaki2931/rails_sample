1e6b30519732f45a6ef8fc44ce60900b5516f62a
sasaki <sasaki2931@gmail.com>
2023-07-28 19:12:10 +0000

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+   # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
+
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
